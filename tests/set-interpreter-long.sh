774bdc0d5ee67dec0f99392d61550ba7b474eac0
Eelco Dolstra <e.dolstra@tudelft.nl>
2005-09-30 15:43:10 +0000

    * Also try a interpreter with a long name.

--- tests/set-interpreter-long.sh
+++ tests/set-interpreter-long.sh
@@ -8,11 +8,16 @@ echo "current interpreter is $oldInterpreter"
 rm -rf scratch
 mkdir -p scratch
 
+newInterpreter=$(pwd)/scratch/iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
 cp simple scratch/
-../src/patchelf --interpreter /oops $(pwd)/scratch/i scratch/simple
+../src/patchelf --interpreter "$newInterpreter" scratch/simple
 
 echo "running with missing interpreter..."
 if scratch/simple; then
     echo "simple works, but it shouldn't"
     exit 1
 fi
+
+echo "running with new interpreter..."
+ln -s "$oldInterpreter" "$newInterpreter"
+scratch/simple
