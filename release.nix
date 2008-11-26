f8a7fd669c381a20595a75a5126aa52814aaa1d7
Eelco Dolstra <e.dolstra@tudelft.nl>
2008-11-26 14:43:56 +0000

    * Different priorities for jobs.

--- release.nix
+++ release.nix
@@ -1,66 +1,83 @@
-let jobs = rec {
+let
 
 
-  tarball =
-    { patchelfSrc ? {path = ./.; rev = 1234;}
-    , nixpkgs ? {path = ../nixpkgs;}
-    , officialRelease ? false
-    }:
+  jobs = rec {
+
+
+    tarball =
+      { patchelfSrc ? {path = ./.; rev = 1234;}
+      , nixpkgs ? {path = ../nixpkgs;}
+      , officialRelease ? false
+      }:
+
+      with import nixpkgs.path {};
+
+      releaseTools.makeSourceTarball {
+        name = "patchelf-tarball";
+        src = patchelfSrc;
+        inherit officialRelease;
+      };
+
+
+    coverage =
+      { tarball ? {path = jobs.tarball {};}
+      , nixpkgs ? {path = ../nixpkgs;}
+      }:
+
+      with import nixpkgs.path {};
+
+      releaseTools.coverageAnalysis {
+        name = "patchelf-coverage";
+        src = tarball;
+      };
+
+
+    build =
+      { tarball ? {path = jobs.tarball {};}
+      , nixpkgs ? {path = ../nixpkgs;}
+      , system ? "i686-linux"
+      }:
+
+      with import nixpkgs.path {inherit system;};
+
+      releaseTools.nixBuild {
+        name = "patchelf-build";
+        src = tarball;
+        postInstall = ''
+          echo "doc readme $out/share/doc/patchelf/README" >> $out/nix-support/hydra-build-products
+        '';
+      };
+
+
+    rpm_fedora9i386 = makeRPM (diskImages: diskImages.fedora9i386) 50;
+    rpm_fedora10i386 = makeRPM (diskImages: diskImages.fedora10i386) 40;
+
     
-    with import nixpkgs.path {};
-    
-    releaseTools.makeSourceTarball {
-      name = "patchelf-tarball";
-      src = patchelfSrc;
-      inherit officialRelease;
-    };
+    deb_debian40i386 = makeDeb (diskImages: diskImages.debian40i386) 30;
+    deb_ubuntu804i386 = makeDeb (diskImages: diskImages.ubuntu804i386) 40;
 
 
-  coverage =
+  };
+
+  
+  makeRPM =
+    diskImageFun: prio:
     { tarball ? {path = jobs.tarball {};}
     , nixpkgs ? {path = ../nixpkgs;}
     }:
 
     with import nixpkgs.path {};
-    
-    releaseTools.coverageAnalysis {
-      name = "patchelf-coverage";
+
+    releaseTools.rpmBuild rec {
+      name = "patchelf-rpm-${diskImage.name}";
       src = tarball;
+      diskImage = diskImageFun vmTools.diskImages;
+      meta = { schedulingPriority = toString prio; };
     };
 
 
-  build =
-    { tarball ? {path = jobs.tarball {};}
-    , nixpkgs ? {path = ../nixpkgs;}
-    , system ? "i686-linux"
-    }:
-
-    with import nixpkgs.path {inherit system;};
-
-    releaseTools.nixBuild {
-      name = "patchelf-build";
-      src = tarball;
-      postInstall = ''
-        echo "doc readme $out/share/doc/patchelf/README" >> $out/nix-support/hydra-build-products
-      '';
-    };
-
-
-  rpm =
-    { tarball ? {path = jobs.tarball {};}
-    , nixpkgs ? {path = ../nixpkgs;}
-    }:
-
-    with import nixpkgs.path {};
-
-    releaseTools.rpmBuild {
-      name = "patchelf-rpm";
-      src = tarball;
-      diskImage = vmTools.diskImages.fedora9i386;
-    };
-
-            
-  deb =
+  makeDeb =
+    diskImageFun: prio:
     { tarball ? {path = jobs.tarball {};}
     , nixpkgs ? {path = ../nixpkgs;}
     }:
@@ -70,8 +87,9 @@ let jobs = rec {
     releaseTools.debBuild {
       name = "patchelf-deb";
       src = tarball;
-      diskImage = vmTools.diskImages.debian40i386;
+      diskImage = diskImageFun vmTools.diskImages;
+      meta = { schedulingPriority = toString prio; };
     };
 
-            
-}; in jobs
+
+in jobs
