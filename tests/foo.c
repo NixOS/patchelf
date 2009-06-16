ecb2b0c96fe83d02d42c75c7ed832721ffd97067
Eelco Dolstra <e.dolstra@tudelft.nl>
2009-06-16 15:26:30 +0000

    * Regression test for NIXPKGS-98: patchelf barfs on dynamic libraries   that contain an .interp section.

--- tests/foo.c
+++ tests/foo.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/* Include a bogus .interp section in libfoo.so (NIXPKGS-98).
+   Borrowed from Glibc. */
+const char __invoke_dynamic_linker__[] __attribute__ ((section (".interp"))) = "/foo/bar";
+
 int bar();
 
 int foo()
