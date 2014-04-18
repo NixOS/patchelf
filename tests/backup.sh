d1672026db0a4715eaf6fde24f32758658a636d7
darealshinji <djcj@gmx.de>
2014-04-18 20:47:11 +0200

    small fixes

--- tests/backup.sh
+++ tests/backup.sh
@@ -7,10 +7,21 @@ mkdir -p ${SCRATCH}
 cp main ${SCRATCH}/
 
 ../src/patchelfmod -d --set-rpath /set/RPath ${SCRATCH}/main
-original=$(md5sum --binary ${SCRATCH}/main | cut -d ' ' -f1)
+if test $(which md5sum) != ""; then
+    original=$(md5sum --binary ${SCRATCH}/main | cut -d ' ' -f1)
+elif test $(which md5) != ""; then
+    original=$(md5 -q ${SCRATCH}/main)
+else
+    echo "no md5 tool found"
+    exit 0
+fi
 
 ../src/patchelfmod -d --backup --set-rpath /set/new/RPath ${SCRATCH}/main
-backup=$(md5sum --binary ${SCRATCH}/main~orig | cut -d ' ' -f1)
+if test $(which md5sum) != ""; then
+    backup=$(md5sum --binary ${SCRATCH}/main~orig | cut -d ' ' -f1)
+elif test $(which md5) != ""; then
+    original=$(md5 -q ${SCRATCH}/main~orig)
+fi
 
 echo "md5 original: $original"
 echo "md5 backup:   $backup"
