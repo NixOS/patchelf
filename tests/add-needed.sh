dee1f214e102d18abde47d3f5374d5f719e20f85
darealshinji <djcj@gmx.de>
2014-04-22 00:40:28 +0200

    *-needed options can now handle comma separated lists; removed *-list options

--- tests/add-needed.sh
+++ tests/add-needed.sh
@@ -8,10 +8,18 @@ mkdir -p ${SCRATCH}/libs
 cp simple ${SCRATCH}/
 cp libfoo.so ${SCRATCH}/libs/
 cp libbar.so ${SCRATCH}/libs/
+cp libfoo.so ${SCRATCH}/libs/libfoo0.so
+cp libbar.so ${SCRATCH}/libs/libbar0.so
 cp libfoo.so ${SCRATCH}/libs/libfoo1.so
 cp libbar.so ${SCRATCH}/libs/libbar1.so
+cp libfoo.so ${SCRATCH}/libs/libfoo2.so
+cp libbar.so ${SCRATCH}/libs/libbar2.so
+cp libfoo.so ${SCRATCH}/libs/libfoo3.so
+cp libbar.so ${SCRATCH}/libs/libbar3.so
 
-../src/patchelfmod -d --add-needed libfoo1.so --add-needed libbar1.so ${SCRATCH}/simple
+../src/patchelfmod -d --add-needed libfoo0.so --add-needed libbar0.so ${SCRATCH}/simple
+../src/patchelfmod -d --add-needed libfoo1.so,libfoo2.so,libfoo3.so ${SCRATCH}/simple
+../src/patchelfmod -d --add-needed libbar1.so,libbar2.so,libbar3.so ${SCRATCH}/simple
 
 export LD_LIBRARY_PATH=$(pwd)/${SCRATCH}/libs
 
