0fa104d5cea8a15db94e07c503f9cbd50483c26d
darealshinji <djcj@gmx.de>
2016-02-06 03:27:52 +0100

    Add a license header
    Because why not?

--- src/patchelf.cc
+++ src/patchelf.cc
@@ -1,3 +1,21 @@
+/*
+ *  PatchELF is a utility to modify properties of ELF executables and libraries
+ *  Copyright (C) 2004-2016  Eelco Dolstra <edolstra@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <string>
 #include <vector>
 #include <set>
