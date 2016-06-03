48143414941d91016efca2b0ef6f32475d6eaa75
Tuomas Tynkkynen <tuomas@tuxera.com>
2016-06-04 00:25:02 +0300

    Add '--allowed-rpath-prefixes' option to '--shrink-rpath'
    Fixes #97. In essence, the problem is that some packages in Nixpkgs have
    RPATHs pointing to both $NIX_BUILD_TOP and $out, e.g.:
    
    /tmp/nix-build-openldap-2.4.44.drv-0/openldap-2.4.44/libraries/libldap_r/.libs
    /tmp/nix-build-openldap-2.4.44.drv-0/openldap-2.4.44/libraries/liblber/.libs
    /nix/store/bfkmdxmv3a3f0g3d2q8jkdz2wam93c5z-openldap-2.4.44/lib
    /nix/store/bfkmdxmv3a3f0g3d2q8jkdz2wam93c5z-openldap-2.4.44/lib64
    
    Currently, running `patchelf --shrink-rpath` does the wrong thing by
    keeping the /tmp/ paths and deleting the /nix/store ones. Now we can fix
    the problem by using
    
    patchelf --shrink-rpath --allowed-rpath-prefixes $NIX_STORE_DIR
    
    in the Nixpkgs fixupPhase instead.

---
+++ tests/shrink-rpath-with-allowed-prefixes.sh
@@ -0,0 +1,47 @@
+#! /bin/sh -e
+SCRATCH=scratch/$(basename $0 .sh)
+
+rm -rf ${SCRATCH}
+mkdir -p ${SCRATCH}
+mkdir -p ${SCRATCH}/libsA
+mkdir -p ${SCRATCH}/libsB
+
+cp main ${SCRATCH}/
+cp libfoo.so libbar.so ${SCRATCH}/libsA/
+cp libfoo.so libbar.so ${SCRATCH}/libsB/
+
+oldRPath=$(../src/patchelf --print-rpath ${SCRATCH}/main)
+if test -z "$oldRPath"; then oldRPath="/oops"; fi
+pathA="$(pwd)/${SCRATCH}/libsA"
+pathB="$(pwd)/${SCRATCH}/libsB"
+../src/patchelf --force-rpath --set-rpath $oldRPath:$pathA:$pathB ${SCRATCH}/main
+
+cp ${SCRATCH}/main ${SCRATCH}/mainA
+cp ${SCRATCH}/main ${SCRATCH}/mainB
+
+../src/patchelf --shrink-rpath ${SCRATCH}/main
+../src/patchelf --shrink-rpath --allowed-rpath-prefixes $oldRPath:$pathA ${SCRATCH}/mainA
+../src/patchelf --shrink-rpath --allowed-rpath-prefixes $oldRPath:$pathB ${SCRATCH}/mainB
+
+check() {
+    exe=$1
+    mustContain=$2
+    mustNotContain=$3
+
+    rpath=$(../src/patchelf --print-rpath $exe)
+    echo "RPATH of $exe after: $rpath"
+
+    if ! echo "$rpath" | grep -q $mustContain; then
+        echo "RPATH didn't contain '$mustContain' when it should have"
+        exit 1
+    fi
+
+    if echo "$rpath" | grep -q $mustNotContain; then
+        echo "RPATH contained '$mustNotContain' when it shouldn't have"
+        exit 1
+    fi
+}
+
+check ${SCRATCH}/main  $pathA $pathB
+check ${SCRATCH}/mainA $pathA $pathB
+check ${SCRATCH}/mainB $pathB $pathA
