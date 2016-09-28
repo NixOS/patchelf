a83eaca427d0bda4cb817c1631871497222e475a
Benjamin Peterson <bp@benjamin.pe>
2016-09-27 22:15:05 -0700

    emit an error if multiple target files are given
    Only one file may be patched at a time. patchelf previously silently ignored
    extra command line arguments. Make extra arguments be an error.

--- src/patchelf.cc
+++ src/patchelf.cc
@@ -1736,6 +1736,7 @@ int mainWrapped(int argc, char * * argv)
     }
 
     if (i == argc) error("missing filename");
+    if (i + 1 < argc) error("only one target filename permitted");
     fileName = argv[i];
 
     patchElf();
