8cc2d64444b0946abb0009058865cd46165b69c4
Eelco Dolstra <edolstra@gmail.com>
2019-09-19 00:06:50 +0200

    flake.nix: Provide a Nixpkgs overlay
    Also update the lock file.

--- flake.nix
+++ flake.nix
@@ -5,6 +5,15 @@
 
   outputs = { self, nixpkgs }: rec {
 
+    overlay = final: prev: {
+
+      patchelf-new = final.stdenv.mkDerivation {
+        name = "patchelf-${hydraJobs.tarball.version}";
+        src = "${hydraJobs.tarball}/tarballs/*.tar.bz2";
+      };
+
+    };
+
     hydraJobs = import ./release.nix {
       patchelfSrc = self;
       nixpkgs = nixpkgs;
@@ -12,7 +21,10 @@
 
     checks.build = hydraJobs.build.x86_64-linux;
 
-    packages.patchelf = hydraJobs.build.x86_64-linux;
+    packages.patchelf = (import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [ self.overlay ];
+    }).patchelf-new;
 
     defaultPackage = packages.patchelf;
 
