NixOS/patchelf

commit 936bae418b77ee9e06a93cd3cd444f4204446973

Allow multiple filenames to patch
This makes behaviour less confusing when multiple filenames are
passed — previously, any extra filenames would be ignored completely,
as would any options passed after a filename. Now these are taken
into account.

src/patchelf.cc
@@ -45,7 +45,7 @@ static bool debugMode = false;
 
 static bool forceRPath = false;
 
-static std::string fileName;
+static std::vector<std::string> fileNames;
 static int pageSize = PAGESIZE;
 
 typedef std::shared_ptr<std::vector<unsigned char>> FileContents;
@@ -1546,7 +1546,7 @@ static bool printNeeded = false;
 static bool noDefaultLib = false;
 
 template<class ElfFile>
-static void patchElf2(ElfFile && elfFile)
+static void patchElf2(ElfFile && elfFile, std::string fileName)
 {
     if (printInterpreter)
         printf("%s\n", elfFile.getInterpreter().c_str());
@@ -1588,17 +1588,19 @@ static void patchElf2(ElfFile && elfFile)
 
 static void patchElf()
 {
-    if (!printInterpreter && !printRPath && !printSoname && !printNeeded)
-        debug("patching ELF file '%s'\n", fileName.c_str());
+    for (auto fileName : fileNames) {
+        if (!printInterpreter && !printRPath && !printSoname && !printNeeded)
+            debug("patching ELF file '%s'\n", fileName.c_str());
 
-    debug("Kernel page size is %u bytes\n", getPageSize());
+        debug("Kernel page size is %u bytes\n", getPageSize());
 
-    auto fileContents = readFile(fileName);
+        auto fileContents = readFile(fileName);
 
-    if (getElfType(fileContents).is32Bit)
-        patchElf2(ElfFile<Elf32_Ehdr, Elf32_Phdr, Elf32_Shdr, Elf32_Addr, Elf32_Off, Elf32_Dyn, Elf32_Sym, Elf32_Verneed>(fileContents));
-    else
-        patchElf2(ElfFile<Elf64_Ehdr, Elf64_Phdr, Elf64_Shdr, Elf64_Addr, Elf64_Off, Elf64_Dyn, Elf64_Sym, Elf64_Verneed>(fileContents));
+        if (getElfType(fileContents).is32Bit)
+            patchElf2(ElfFile<Elf32_Ehdr, Elf32_Phdr, Elf32_Shdr, Elf32_Addr, Elf32_Off, Elf32_Dyn, Elf32_Sym, Elf32_Verneed>(fileContents), fileName);
+        else
+            patchElf2(ElfFile<Elf64_Ehdr, Elf64_Phdr, Elf64_Shdr, Elf64_Addr, Elf64_Off, Elf64_Dyn, Elf64_Sym, Elf64_Verneed>(fileContents), fileName);
+    }
 }
 
 
@@ -1721,11 +1723,12 @@ int mainWrapped(int argc, char * * argv)
             printf(PACKAGE_STRING "\n");
             return 0;
         }
-        else break;
+        else {
+            fileNames.push_back(arg);
+        }
     }
 
-    if (i == argc) error("missing filename");
-    fileName = argv[i];
+    if (fileNames.empty()) error("missing filename");
 
     patchElf();
 

commit 711a20dcbf35c3fefc629c62016e93547c3b7762

Update doc to describe multiple filename args allowed
Commit 936bae418b77ee9e06a93cd3cd444f4204446973 added support to handle
several files at once, but forgot to update the man and help output to
describe it.

Fixes: 936bae418b77ee9e06a93cd3cd444f4204446973

patchelf.1
@@ -10,6 +10,7 @@ patchelf - Modify ELF files
 .I OPTION
 .B
 .I FILE
+.SM ...
 .B
 
 .SH DESCRIPTION
@@ -20,7 +21,7 @@ of executables and change the RPATH of executables and libraries.
 
 .SH OPTIONS
 
-The single option given operates on a given FILE, editing in place.
+The single option given operates on each FILE, editing in place.
 
 .IP "--page-size SIZE"
 Uses the given page size instead of the default.

src/patchelf.cc
@@ -1646,7 +1646,7 @@ void showHelp(const std::string & progName)
   [--no-default-lib]\n\
   [--debug]\n\
   [--version]\n\
-  FILENAME\n", progName.c_str());
+  FILENAME...\n", progName.c_str());
 }
 
 