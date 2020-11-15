62e39e05dc79556ad3ea218a7f1fcd42dd2ed131
Julian Stecklina <js@alien8.de>
2020-11-15 14:39:22 +0100

    Add ELF test case with empty string table
    Parsing this file results in patchelf triggering an assertion:
    
    patchelf: patchelf.cc:439: ElfFile<Elf64_Ehdr, Elf64_Phdr, Elf64_Shdr, unsigned long, unsigned long, Elf64_Dyn, Elf64_Sym, Elf64_Verneed, unsigned short>::ElfFile(FileContents) [Elf_Ehdr = Elf64_Ehdr, Elf_Phdr = Elf64_Phdr, Elf_Shdr = Elf64_Shdr, Elf_Addr = unsigned long, Elf_Off = unsigned long, Elf_Dyn = Elf64_Dyn, Elf_Sym = Elf64_Sym, Elf_Verneed = Elf64_Verneed, Elf_Versym = unsigned short]: Assertion `shstrtabSize > 0' failed.
    Aborted (core dumped)

--- tests/invalid-elf.sh
+++ tests/invalid-elf.sh
@@ -15,7 +15,7 @@ TEST_DIR=$(dirname $(readlink -f $0))/invalid-elf
 
 # Each test case is listed here. The names should roughly indicate
 # what makes the given ELF file invalid.
-TEST_CASES="invalid-shrstrtab-idx invalid-shrstrtab-size"
+TEST_CASES="invalid-shrstrtab-idx invalid-shrstrtab-size invalid-shrstrtab-zero"
 
 FAILED_TESTS=""
 
