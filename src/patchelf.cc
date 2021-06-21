NixOS/patchelf

commit 4efbce410d00c8cb43f134181d07b364bcf78022

merge from PR243

src/patchelf.cc
@@ -772,12 +772,15 @@ void ElfFile<ElfFileParamNames>::rewriteSectionsLibrary()
        PT_LOAD segment located directly after the last virtual address
        page of other segments. */
     Elf_Addr startPage = 0;
+    Elf_Addr firstPage = 0;
     for (auto & phdr : phdrs) {
         Elf_Addr thisPage = roundUp(rdi(phdr.p_vaddr) + rdi(phdr.p_memsz), getPageSize());
         if (thisPage > startPage) startPage = thisPage;
+        if (rdi(phdr.p_type) == PT_PHDR) firstPage = rdi(phdr.p_vaddr) - rdi(phdr.p_offset);
     }
 
     debug("last page is 0x%llx\n", (unsigned long long) startPage);
+    debug("first page is 0x%llx\n", (unsigned long long) firstPage);
 
     /* When normalizing note segments we will in the worst case be adding
        1 program header for each SHT_NOTE section. */
@@ -848,7 +851,7 @@ void ElfFile<ElfFileParamNames>::rewriteSectionsLibrary()
     assert(curOff == startOffset + neededSpace);
 
     /* Write out the updated program and section headers */
-    rewriteHeaders(rdi(hdr->e_phoff));
+    rewriteHeaders(firstPage + hdr->e_phoff);
 }
 
 

commit 83eab25c30c8d3a12ef2502493ca602747ff9897

rewriteSectionsLibrary: Fix endianness issue
Currently, patchelf creats a broken PPC binary in
tests/no-rpath-pie-powerpc.sh:

$ readelf -l scratch/no-rpath-pie-powerpc/no-rpath
[...]
Program Headers:
  Type           Offset   VirtAddr   PhysAddr   FileSiz MemSiz  Flg Align
  PHDR           0x000034 0x34000000 0x34000000 0x00120 0x00120 R   0x4
[...]

That happens because 4efbce41 lost endianness conversion
for rewriteSectionsLibrary. This commit puts it back.

Fixes: 4efbce410d00c8cb43f134181d07b364bcf78022

src/patchelf.cc
@@ -851,7 +851,7 @@ void ElfFile<ElfFileParamNames>::rewriteSectionsLibrary()
     assert(curOff == startOffset + neededSpace);
 
     /* Write out the updated program and section headers */
-    rewriteHeaders(firstPage + hdr->e_phoff);
+    rewriteHeaders(firstPage + rdi(hdr->e_phoff));
 }
 
 