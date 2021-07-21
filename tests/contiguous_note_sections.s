d2bb23badf3170c69f0377e7acc66fa0871bd435
Ovidiu Panait <ovidiu.panait@windriver.com>
2021-07-21 22:31:07 +0300

    tests: add testcase for alignment issues with contiguous note sections
    Add a testcase for the following reported alignment issue with contiguous note
    sections (#275):
    
    """
    If a binary has multiple SHT_NOTE sections and corresponding PT_NOTE
    headers, we can see the error:
    
    patchelf: cannot normalize PT_NOTE segment: non-contiguous SHT_NOTE sections
    
    if the SHT_NOTE sections aren't sized to end on aligned boundaries. An example
    would be a binary with:
    
    [ 2] .note.ABI-tag NOTE 00000000000002f4 000002f4
    0000000000000020 0000000000000000 A 0 0 4
    [ 3] .note.gnu.propert NOTE 0000000000000318 00000318
    0000000000000030 0000000000000000 A 0 0 8
    [ 4] .note.gnu.build-i NOTE 0000000000000348 00000348
    0000000000000024 0000000000000000 A 0 0 4
    
    NOTE 0x0000000000000318 0x0000000000000318 0x0000000000000318
    0x0000000000000030 0x0000000000000030 R 0x8
    NOTE 0x00000000000002f4 0x00000000000002f4 0x00000000000002f4
    0x0000000000000078 0x0000000000000074 R 0x4
    
    since the PT_NOTE section at 2f4 covers [2] and [3] but the code
    calclates curr_off should be 314, not the 318 in the binary. This
    is an alignment issue.
    """
    
    Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>

---
+++ tests/contiguous_note_sections.s
@@ -0,0 +1,23 @@
+/*
+ * Testcase for error:
+ * patchelf: cannot normalize PT_NOTE segment: non-contiguous SHT_NOTE sections
+ */
+.section ".note.my-section0", "a", @note
+    .align 4
+    .long 1f - 0f              /* name length (not including padding) */
+    .long 3f - 2f              /* desc length (not including padding) */
+    .long 1                    /* type = NT_VERSION */
+0:  .asciz "my-version-12345" /* name */
+1:  .align 4
+2:  .long 1                    /* desc - toolchain version number, 32-bit LE */
+3:  .align 4
+
+.section ".note.my-section1", "a", @note
+    .align 8
+    .long 1f - 0f              /* name length (not including padding) */
+    .long 3f - 2f              /* desc length (not including padding) */
+    .long 1                    /* type = NT_VERSION */
+0:  .asciz "my-version-1" /* name */
+1:  .align 4
+2:  .long 1                    /* desc - toolchain version number, 32-bit LE */
+3:  .align 4
