NixOS/patchelf

commit fce77b7cd8aeac2cec7c4bd1d14e088997786588

replace for loop with any_of
Signed-off-by: Rosen Penev <rosenp@gmail.com>

src/patchelf.cc
@@ -79,9 +79,7 @@ static std::vector<std::string> splitColonDelimitedString(const char * s)
 
 static bool hasAllowedPrefix(const std::string & s, const std::vector<std::string> & allowedPrefixes)
 {
-    for (auto & i : allowedPrefixes)
-        if (!s.compare(0, i.size(), i)) return true;
-    return false;
+    return std::any_of(allowedPrefixes.begin(), allowedPrefixes.end(), [&](const std::string & i) { return !s.compare(0, i.size(), i); });
 }
 
 
@@ -787,10 +785,8 @@ void ElfFile<ElfFileParamNames>::rewriteSectionsLibrary()
 
     /* When normalizing note segments we will in the worst case be adding
        1 program header for each SHT_NOTE section. */
-    unsigned int num_notes = 0;
-    for (const auto & shdr : shdrs)
-        if (rdi(shdr.sh_type) == SHT_NOTE)
-            num_notes++;
+    unsigned int num_notes = std::count_if(shdrs.begin(), shdrs.end(), [this](Elf_Shdr shdr) { return rdi(shdr.sh_type) == SHT_NOTE; });
+    ;
 
     /* Because we're adding a new section header, we're necessarily increasing
        the size of the program header table.  This can cause the first section
@@ -992,11 +988,7 @@ void ElfFile<ElfFileParamNames>::normalizeNoteSegments()
        one of them has to be replaced. */
 
     /* We don't need to do anything if no note segments were replaced. */
-    bool replaced_note = false;
-    for (const auto & i : replacedSections) {
-        if (rdi(findSection(i.first).sh_type) == SHT_NOTE)
-            replaced_note = true;
-    }
+    bool replaced_note = std::any_of(replacedSections.begin(), replacedSections.end(), [this](std::pair<const std::string, std::string> & i) { return rdi(findSection(i.first).sh_type) == SHT_NOTE; });
     if (!replaced_note) return;
 
     size_t orig_count = phdrs.size();
@@ -1604,8 +1596,7 @@ void ElfFile<ElfFileParamNames>::addNeeded(const std::set<std::string> & libs)
     auto shdrDynStr = findSection(".dynstr");
 
     /* add all new libs to the dynstr string table */
-    unsigned int length = 0;
-    for (auto & i : libs) length += i.size() + 1;
+    unsigned int length = std::count_if(libs.begin(), libs.end(), [](const std::string & lib) { return lib.size() + 1; });
 
     std::string & newDynStr = replaceSection(".dynstr",
         rdi(shdrDynStr.sh_size) + length + 1);

commit f533f7b8987648286a587d25c33b12be8ff5f0c0

addNeeded: fix assertion triggered due to bad .dynstr section resize
When running "--add-needed" subcommand on a hello world binary, the
following assertion is triggered:
"""
$ echo "int main() {}" | gcc -xc -o test -
$ patchelf --add-needed foo.so --output /dev/null test
patching ELF file 'scratch/plain-needed/main'
patchelf: patchelf.cc:1167: void setSubstr(std::string&, unsigned int, const string&): Assertion `pos + t.size() <= s.size()' failed.
Aborted (core dumped)
"""

This is due to the fact that .dynstr section is resized incorrectly:
"""
    unsigned int length = std::count_if(libs.begin(), libs.end(),
        [](const std::string & lib) { return lib.size() + 1; });
"""

std::count_if() will return the number of strings in std::set<std::string> libs
(e.g. 1 in the foo.so example). However, in order to properly resize the
.dynstr section, subsequent code expects the size (in bytes) of all the strings
that are to be appended:
"""
    std::string & newDynStr = replaceSection(".dynstr",
        rdi(shdrDynStr.sh_size) + length + 1);
"""

To fix this, iterate over "libs" and compute the length of all the strings that
need to be added to the .dynstr section.

Fixes #291.

Fixes: fce77b7cd8ae ("replace for loop with any_of")
Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>

src/patchelf.cc
@@ -1592,12 +1592,15 @@ void ElfFile<ElfFileParamNames>::addNeeded(const std::set<std::string> & libs)
     auto shdrDynamic = findSection(".dynamic");
     auto shdrDynStr = findSection(".dynstr");
 
+    unsigned int length = 0;
+
     /* add all new libs to the dynstr string table */
-    unsigned int length = std::count_if(libs.begin(), libs.end(),
-        [](const std::string & lib) { return lib.size() + 1; });
+    for (auto &lib : libs)
+        length += lib.size() + 1;
 
     std::string & newDynStr = replaceSection(".dynstr",
         rdi(shdrDynStr.sh_size) + length + 1);
+
     std::set<Elf64_Xword> libStrings;
     unsigned int pos = 0;
     for (auto & i : libs) {