094022f06c588cb6072932031615a3cf7ab175b8
Joerie de Gram <j.de.gram@gmail.com>
2021-08-09 19:16:47 +0200

    tests/contiguous_note_sections.s: use alternate section type prefix
    fixes #294

--- tests/contiguous_note_sections.s
+++ tests/contiguous_note_sections.s
@@ -2,7 +2,7 @@
  * Testcase for error:
  * patchelf: cannot normalize PT_NOTE segment: non-contiguous SHT_NOTE sections
  */
-.section ".note.my-section0", "a", @note
+.section ".note.my-section0", "a", %note
     .align 4
     .long 1f - 0f              /* name length (not including padding) */
     .long 3f - 2f              /* desc length (not including padding) */
@@ -12,7 +12,7 @@
 2:  .long 1                    /* desc - toolchain version number, 32-bit LE */
 3:  .align 4
 
-.section ".note.my-section1", "a", @note
+.section ".note.my-section1", "a", %note
     .align 8
     .long 1f - 0f              /* name length (not including padding) */
     .long 3f - 2f              /* desc length (not including padding) */
