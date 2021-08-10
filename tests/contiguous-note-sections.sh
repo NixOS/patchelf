9e364e5fcaa7eda3bd140104b41bed1565d1e2e6
Jörg Thalheim <joerg@thalheim.io>
2021-08-10 14:03:24 +0200

    tests/contiguous-note-sections: rename for consistency

--- tests/contiguous-note-sections.sh
+++ tests/contiguous-note-sections.sh
@@ -3,4 +3,4 @@
 # Running --set-interpreter on this binary should not produce the following
 # error:
 # patchelf: cannot normalize PT_NOTE segment: non-contiguous SHT_NOTE sections
-../src/patchelf --set-interpreter ld-linux-x86-64.so.2 contiguous_note_sections
+../src/patchelf --set-interpreter ld-linux-x86-64.so.2 contiguous-note-sections
