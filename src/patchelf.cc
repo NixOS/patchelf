411e004f48f0c0560431be606e31a46c7dce2214
Jörg Thalheim <joerg@thalheim.io>
2021-09-12 10:26:13 +0200

    fix setting empty rpath

--- src/patchelf.cc
+++ src/patchelf.cc
@@ -1479,7 +1479,7 @@ void ElfFile<ElfFileParamNames>::modifyRPath(RPathOp op,
     debug("new rpath is '%s'\n", newRPath.c_str());
 
 
-    if (newRPath.size() <= rpathSize) {
+    if (rpath && newRPath.size() <= rpathSize) {
         memcpy(rpath, newRPath.c_str(), newRPath.size() + 1);
         return;
     }
