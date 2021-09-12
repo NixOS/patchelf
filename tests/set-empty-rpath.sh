de3901853d49e7a607fc2bcdeb18e33875bce8ce
Jörg Thalheim <joerg@thalheim.io>
2021-09-12 10:26:13 +0200

    fix setting empty rpath

---
+++ tests/set-empty-rpath.sh
@@ -0,0 +1,11 @@
+#! /bin/sh -e
+SCRATCH=scratch/$(basename $0 .sh)
+
+rm -rf ${SCRATCH}
+mkdir -p ${SCRATCH}
+
+cp simple ${SCRATCH}/simple
+
+../src/patchelf --force-rpath --set-rpath "" ${SCRATCH}/simple
+
+${SCRATCH}/simple
