90da38b8cfd49dbb653e2976003e8ee08f2c39d2
Jörg Thalheim <joerg@thalheim.io>
2021-09-13 17:55:05 +0200

    grow-test: fix on non-gcc systems

--- tests/grow-file.sh
+++ tests/grow-file.sh
@@ -5,12 +5,12 @@ SCRATCH=scratch/$(basename $0 .sh)
 rm -rf ${SCRATCH}
 mkdir -p ${SCRATCH}
 
-g++ -x c -fPIC -pie -o ${SCRATCH}/simple simple.c
+cp simple-pie ${SCRATCH}/simple-pie
 
 # Add a 40MB rpath
 head -c 40000000 /dev/urandom > ${SCRATCH}/foo.bin
 
 # Grow the file
-../src/patchelf --add-rpath @${SCRATCH}/foo.bin ${SCRATCH}/simple
+../src/patchelf --add-rpath @${SCRATCH}/foo.bin ${SCRATCH}/simple-pie
 # Make sure we can still run it
-${SCRATCH}/simple
+${SCRATCH}/simple-pie
