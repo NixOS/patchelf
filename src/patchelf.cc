761b494a9f5ed9af8a5033acec862ea547cdf0bd
Emery Hemingway <ehmry@posteo.net>
2022-05-20 09:37:02 -0500

    modifyRPath: return early if new and old rpath are empty

--- src/patchelf.cc
+++ src/patchelf.cc
@@ -1350,8 +1350,8 @@ void ElfFile<ElfFileParamNames>::modifyRPath(RPathOp op,
     debug("new rpath is '%s'\n", newRPath.c_str());
 
 
-    if (rpath && newRPath.size() <= rpathSize) {
-        memcpy(rpath, newRPath.c_str(), newRPath.size() + 1);
+    if (newRPath.size() <= rpathSize) {
+        if (rpath) memcpy(rpath, newRPath.c_str(), newRPath.size() + 1);
         return;
     }
 
