c00676a28af82f4c73c19916b803d7058f02d104
Christian Göttsche <cgzones@googlemail.com>
2023-02-21 19:40:57 +0100

    Close file before potentially throwing

--- src/patchelf.cc
+++ src/patchelf.cc
@@ -196,10 +196,10 @@ static FileContents readFile(const std::string & fileName,
     while ((portion = read(fd, contents->data() + bytesRead, size - bytesRead)) > 0)
         bytesRead += portion;
 
+    close(fd);
+
     if (bytesRead != size)
         throw SysError(fmt("reading '", fileName, "'"));
-
-    close(fd);
 
     return contents;
 }
