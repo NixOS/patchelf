eb9ea0094bd7e2a278c0d57cb5d8b032a13fc5e4
Breno Rodrigues Guimaraes <brenorg@gmail.com>
2023-02-23 22:15:12 -0300

    Shellcheck fixes

--- tests/repeated-updates.sh
+++ tests/repeated-updates.sh
@@ -1,16 +1,16 @@
 #! /bin/sh -e
 
-SCRATCH=scratch/$(basename $0 .sh)
+SCRATCH=scratch/$(basename "$0" .sh)
 PATCHELF=$(readlink -f "../src/patchelf")
 
-rm -rf ${SCRATCH}
-mkdir -p ${SCRATCH}
+rm -rf "${SCRATCH}"
+mkdir -p "${SCRATCH}"
 
-cp simple ${SCRATCH}/
-cp libfoo.so ${SCRATCH}/
-cp libbar.so ${SCRATCH}/
+cp simple "${SCRATCH}/"
+cp libfoo.so "${SCRATCH}/"
+cp libbar.so "${SCRATCH}/"
 
-cd ${SCRATCH}
+cd "${SCRATCH}"
 
 ${PATCHELF} --add-needed ./libbar.so simple
 
@@ -20,7 +20,7 @@ ${PATCHELF} --add-needed ./libbar.so simple
 ###############################################################################
 load_segments_before=$(readelf -W -l libbar.so | grep -c LOAD)
 
-for i in $(seq 1 100)
+for _ in $(seq 1 100)
 do
     ${PATCHELF} --set-soname ./libbar.so libbar.so
     ${PATCHELF} --set-soname libbar.so libbar.so
@@ -33,7 +33,7 @@ load_segments_after=$(readelf -W -l libbar.so | grep -c LOAD)
 # To be even more strict, check that we don't add too many extra LOAD entries
 ###############################################################################
 echo "Segments before: ${load_segments_before} and after: ${load_segments_after}"
-if [ ${load_segments_after} -gt ${load_segments_before} + 2 ]
+if [ "${load_segments_after}" -gt $((load_segments_before + 2)) ]
 then
     exit 1
 fi
