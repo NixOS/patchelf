551ffcd55d8097b128e6c5295a53f5709f23e8a0
mayeut <mayeut@users.noreply.github.com>
2023-04-29 13:38:37 +0200

    chore(CI): run tests on different distributions

--- tests/set-interpreter-long.sh
+++ tests/set-interpreter-long.sh
@@ -6,7 +6,12 @@ SCRATCH=scratch/$(basename "$0" .sh)
 oldInterpreter=$(../src/patchelf --print-interpreter ./simple)
 echo "current interpreter is $oldInterpreter"
 
-if test "$(uname)" = Linux; then
+RUN_EXPLICIT_INTERPRETER=0
+if [ "$(uname)" = Linux ] && ! grep qemu /proc/1/cmdline >/dev/null 2>&1; then  # QEMU & ldd/ld.so are not playing well together in certain cases
+    RUN_EXPLICIT_INTERPRETER=1
+fi
+
+if [ "${RUN_EXPLICIT_INTERPRETER}" -ne 0 ]; then
     echo "running with explicit interpreter..."
     "$oldInterpreter" ./simple
 fi
@@ -28,7 +33,7 @@ echo "running with new interpreter..."
 ln -s "$oldInterpreter" "$newInterpreter"
 "${SCRATCH}"/simple
 
-if test "$(uname)" = Linux; then
+if [ "${RUN_EXPLICIT_INTERPRETER}" -ne 0 ]; then
     echo "running with explicit interpreter..."
     "$oldInterpreter" "${SCRATCH}/simple"
 fi
