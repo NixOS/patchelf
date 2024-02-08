da4b0b00ae73398e4ecf41bc5dc3680f16dffeea
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-02-08 17:56:30 +0100

    Allocate PHT & SHT at the end of the *.elf

--- tests/short-first-segment.sh
+++ tests/short-first-segment.sh
@@ -12,7 +12,7 @@ if ! gzip --version >/dev/null; then
 fi
 
 if test "$(uname -i)" != x86_64 || test "$(uname)" != Linux; then
-    echo "skipping test: not supported on x86_64 Linux"
+    echo "skipping test: supported only on x86_64 Linux"
     exit 77
 fi
 
@@ -24,8 +24,10 @@ cd "${SCRATCH}"
 
 ldd "${EXEC_NAME}"
 
-${PATCHELF} --add-rpath lalalalalalalala --output modified1 "${EXEC_NAME}"
+${PATCHELF} --set-rpath $(tr -dc A-Za-z0-9 </dev/urandom | head -c 4096) --output modified1 "${EXEC_NAME}"
+${PATCHELF} --add-rpath $(tr -dc A-Za-z0-9 </dev/urandom | head -c 4096) modified1
+
 ldd modified1
 
-${PATCHELF}  --add-needed "libXcursor.so.1" --output modified2 modified1
+${PATCHELF} --add-needed "libXcursor.so.1" --output modified2 modified1
 ldd modified2
