c05aa8e6d000f412bf840b44bb36003d3c403935
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-12-17 18:40:23 +0100

    Allocate PHT & SHT at the end of the *.elf file

--- tests/grow-file.sh
+++ tests/grow-file.sh
@@ -7,10 +7,11 @@ mkdir -p "${SCRATCH}"
 
 cp simple-pie "${SCRATCH}/simple-pie"
 
-# Add a 40MB rpath
-tr -cd 'a-z0-9' < /dev/urandom | dd count=40 bs=1000000 > "${SCRATCH}/foo.bin"
+# Add a large rpath
+printf '=%.0s' $(seq 1 4096) > "${SCRATCH}/foo.bin"
 
 # Grow the file
 ../src/patchelf --add-rpath @"${SCRATCH}/foo.bin" "${SCRATCH}/simple-pie"
+
 # Make sure we can still run it
 "${SCRATCH}/simple-pie"
