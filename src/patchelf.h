43b75fbc9ffbc1190fee7c8693ad74cb8286cfd4
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-12-28 17:00:44 +0100

    Allocate PHT & SHT at the end of the *.elf file

--- src/patchelf.h
+++ src/patchelf.h
@@ -120,7 +120,8 @@ private:
     std::string & replaceSection(const SectionName & sectionName,
         unsigned int size);
 
-    [[nodiscard]] bool haveReplacedSection(const SectionName & sectionName) const;
+    [[nodiscard]] bool hasReplacedSection(const SectionName & sectionName) const;
+    [[nodiscard]] bool canReplaceSection(const SectionName & sectionName) const;
 
     void writeReplacedSections(Elf_Off & curOff,
         Elf_Addr startAddr, Elf_Off startOffset);
